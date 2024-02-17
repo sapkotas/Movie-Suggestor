00632858da3e9cd4e3509fb66f49f7099d76be3d
Sunav Sapkota <sunavsapkota57@gmail.com>
2024-02-17 18:38:06 +0545

    updated

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -35,6 +35,6 @@ const Profile = () => {
     localStorage.removeItem("accessToken");
     navigate("/login");
   };
-  return <button onClick={onLogout}>Logout</button>;
+  return <button onClick={onLogout}>Logout</button>;hello
 };
 export default Profile;
