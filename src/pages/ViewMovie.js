76d0f5358b4f39056b112322283829e634a184d8
Sunav Sapkota <sunavsapkota57@gmail.com>
2024-03-02 21:30:32 +0545

    modified ViewMovie page

--- src/pages/ViewMovie.js
+++ src/pages/ViewMovie.js
@@ -3,29 +3,28 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ViewMovie = () => {
-  const getparams = useParams();
-  const getId = getparams.id;
+  const { id } = useParams();
   const [movieDetails, setMovieDetails] = useState({});
 
   useEffect(() => {
-    getSingleMovieInfo();
-  }, []);
+    const getSingleMovieInfo = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.dynoacademy.com/test-api/v1/movie/${id}`
+        );
+        console.log("Response:", response); // Logging the response
+        setMovieDetails(response.data.singleMovieData);
+      } catch (error) {
+        console.error("Error fetching movie details:", error); // Logging the error
+        alert("Error found in the page");
+      }
+    };
 
-  const getSingleMovieInfo = async () => {
-    try {
-      const response = await axios.get(
-        `https://api.dynoacademy.com/test-api/v1/movie/${getId}`
-      );
-      setMovieDetails(response.data.singleMovieData);
-    } catch (error) {
-      alert("Error found in the page");
-    }
-  };
+    getSingleMovieInfo(); // Call the function directly inside useEffect
+  }, [id]); // Include id as a dependency
+
   return (
     <>
-      {/* This is viewPage {getId}
-        <button onClick={getSingleMovieInfo}>This is movie page</button>
-        <br /> */}
       <b> Movie Details : </b> <br />
       Movie Name :{movieDetails.name} <br /> <br />
       Info :{movieDetails.info};<br /> <br />
