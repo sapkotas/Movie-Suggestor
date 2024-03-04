e47484daabaa34a892d8ff7efab38a9a88979e1e
Sunav Sapkota <sunavsapkota57@gmail.com>
2024-03-04 14:33:47 +0545

    changes

--- src/pages/AddMovie.js
+++ src/pages/AddMovie.js
@@ -1,63 +1,90 @@
 import axios from "axios";
 import { useRef } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import MovieNavBar from "../components/MovieNavBar";
 
 const AddMovie = () => {
-  const movierefrence = useRef();
-  const ratingrefrence = useRef();
-  const descrefrence = useRef();
+  const history = useHistory();
 
-  const navigate = useNavigate();
+  const movie_name_reference = useRef();
+  const rating_reference = useRef();
+  const desc_reference = useRef();
+
   const addMovieHandler = async (e) => {
     e.preventDefault();
 
     const movieData = {
-      movie_name: movierefrence.current.value,
-      rating: ratingrefrence.current.value,
-      description: descrefrence.current.value,
+      movie_name: movie_name_reference.current.value,
+      rating: rating_reference.current.value,
+      description: desc_reference.current.value,
     };
 
     try {
       const response = await axios.post(
-        "https://api.dynoacademy.com/test-api/v1/movies ",
+        "https://api.dynoacademy.com/test-api/v1/movies",
         movieData,
         {
-          timeout: 1000,
+          timeout: 10000,
         }
       );
       alert(response.data.message);
-      navigate("/", { replace: true });
+      history.replace("/");
     } catch (error) {
       if (error.response) {
         alert(error.response.data.errors[0].message);
       } else {
-        alert("Unknown error occured!");
+        alert("Unknown error occoured! Try again later.");
       }
     }
   };
+
   return (
     <>
-      <b>This is Add Movie page</b>
-      <br />
-      <Link to="/">Home</Link>
+      <MovieNavBar />
 
-      <form onSubmit={addMovieHandler}>
-        Movie Name: <br />
-        <input type="text" placeholder="Movie Name" ref={movierefrence} />
+      <Container>
         <br />
-        <br />
-        Rating: <br />
-        <input type="text" placeholder="Rating" ref={ratingrefrence} />
-        <br />
-        <br />
-        Description:
-        <br />
-        <textarea placeholder="Description" ref={descrefrence} />
-        <br />
-        <br />
-        <button type="submit">Add a Movie</button>
-      </form>
+        <form onSubmit={addMovieHandler}>
+          <h3>Add a movie: </h3>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Movie name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter movie name"
+              ref={movie_name_reference}
+              autoComplete={false}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Rating</Form.Label>
+            <Form.Control
+              type="number"
+              ref={rating_reference}
+              autoComplete={false}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Description</Form.Label>
+
+            <Form.Control
+              as="textarea"
+              placeholder="Description"
+              ref={desc_reference}
+              style={{ height: "100px" }}
+            />
+          </Form.Group>
+
+          <Button variant="dark" type="submit">
+            Add a Movie
+          </Button>
+        </form>
+      </Container>
     </>
   );
 };
+
 export default AddMovie;
